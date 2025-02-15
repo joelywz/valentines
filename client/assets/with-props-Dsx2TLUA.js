3e314dd310fb8b712bcd25082aeb56929cf440e1
Joel Yong <yongwenzhuo@gmail.com>
2025-02-15 19:24:14 +0800

    Updates

---
+++ client/assets/with-props-Dsx2TLUA.js
@@ -0,0 +1,1 @@
+import{t as p,v as t,w as e,x as o,r as s,y as u}from"./chunk-IR6S3I6Y-Cflp3U6u.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
